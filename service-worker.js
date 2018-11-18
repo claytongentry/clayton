9b40dec3f21e48432b67dcdff7adda16e6fbb984
claytongentry <claytongentry2017@u.northwestern.edu>
2018-11-17 23:19:35 -0500

    update favicon

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f8cc5fdd46c72c5be279c459c8af242.js"
+  "/precache-manifest.63de2111ee1b9a11577be0114b1391d3.js"
 );
 
 workbox.clientsClaim();
