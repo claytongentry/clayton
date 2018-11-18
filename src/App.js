c4008c7f29ccae9fde0b6b5300d61040df078f2d
claytongentry <claytongentry2017@u.northwestern.edu>
2018-11-17 23:02:28 -0500

    rm ga import

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-60920026-1');
-
 const APP_BLACK = "#47525E"
 const APP_WHITE = "#F7F7F7"
 
