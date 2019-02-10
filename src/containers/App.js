6877e8dd2e6767125ee6f6a932f26b0ac6f4afe4
claytongentry <claytongentry2017@u.northwestern.edu>
2019-02-10 18:21:18 -0500

    add icons

--- src/containers/App.js
+++ src/containers/App.js
@@ -35,17 +35,41 @@ class App extends Component {
         {/* Banner Card */}
         <Card style={{ marginLeft: 110, marginRight: 110 }}>
           <Title style={{ color: APP_WHITE, fontSize: 120, marginLeft: 160 }}>
-            HEY
+            HEY,
           </Title>
           <Title style={{ color: APP_WHITE, fontSize: 36, textAlign: "center"}}>
-            I'M CLAYTON
+            I'M CLAYTON.
           </Title>
           <Text style={{ color: APP_WHITE, textAlign: "center" }}>
-            I write software and drink coffee. And I’m all out of coffee.
+            I write software and drink coffee.<br/>And I’m all out of coffee.
           </Text>
           <Text style={{ fontSize: 10, color: APP_WHITE, textAlign: "center" }}>
             New York, NY
           </Text>
+
+          <div style={{ textAlign: "center" }}>
+            <Icon
+              name="fa-linkedin-in"
+              href="https://www.linkedin.com/in/clayton-gentry-6492588a/"
+            />
+
+            <Icon
+              name="fa-github"
+              href="https://github.com/claytongentry/"
+            />
+
+            <Icon
+              name="fa-instagram"
+              href="https://www.instagram.com/cg3ntry"
+            />
+
+            <Icon
+              name="fas fa-envelope"
+              href="mailto:cg3ntry@gmail.com"
+            />
+
+          </div>
+
         </Card>
       </div>
     )
@@ -62,5 +86,12 @@ class App extends Component {
     this.setState({ viewport: { innerHeight, innerWidth }})
   }
 }
+
+const Icon = (props) =>
+  <a href={props.href} target="_blank">
+    <i className={`fab ${props.name} fa-2x`} style={{ color: APP_WHITE, padding: 50 }}/>
+  </a>
+
+
 
 export default App
