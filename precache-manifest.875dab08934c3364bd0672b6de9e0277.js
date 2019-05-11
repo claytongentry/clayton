a26f522a0c2846a61911ff6ae315145ffc300995
claytongentry <claytongentry2017@u.northwestern.edu>
2019-05-11 13:33:29 -0400

    fix up pursuit card out buttons

---
+++ precache-manifest.875dab08934c3364bd0672b6de9e0277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836f83e907fe863ec5df89506bd84633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05bb030c9f589ac941d",
+    "url": "/static/css/main.924fd6ea.chunk.css"
+  },
+  {
+    "revision": "ab8ba749a6c7d10eb21e",
+    "url": "/static/js/2.9bdf3c1f.chunk.js"
+  },
+  {
+    "revision": "c05bb030c9f589ac941d",
+    "url": "/static/js/main.7ce30dd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
