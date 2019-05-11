0e9df57ed632d2d868a5f47e966bb522256939d3
claytongentry <claytongentry2017@u.northwestern.edu>
2019-05-11 13:33:29 -0400

    fix up pursuit card out buttons

--- src/containers/Pursuits.js
+++ src/containers/Pursuits.js
@@ -8,68 +8,59 @@ class Pursuits extends Component {
     super(props)
 
     this.state = {
-      pursuits: [
-        {
-          title: "Inverse",
-          body: [
-            {
-              type: "graf",
-              text: "Inverse is a media and entertainment site dedicated to sparking curiosity about the future."
-            },
-            {
-              type: "graf",
-              text: "For three years, I've worked with a small but all-star engineering team to build infrastructure that handles tens of millions of pageviews every month without breaking a sweat."
-            }
-          ],
-          link: "https://www.inverse.com"
-        },
-        {
-          title: "Watch Design",
-          body: [
-            {
-              type: "graf",
-              text: "I love designing watches, and visual design generally. Here's one I designed recently, inspired by a trip to Naples, Florida."
-            },
-            {
-              type: "image",
-              link: "https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg",
-              alt: "cat"
-            },
-            {
-              type: "graf",
-              text: "My 2019 New Year's Resolution is to actually make one."
-            }
-          ]
-        },
-        {
-          title: "Open Source",
-          body: []
-        }
-      ]
+      pursuits: null
     }
   }
 
+  componentDidMount() {
+    fetch("data/pursuits.json")
+    .then(resp => resp.json())
+    .then(pursuits => this.setState({ pursuits }))
+    .catch(err => console.error(err))
+  }
+
   render() {
+    const { pursuits } = this.state
+
+    if (!pursuits) {
+      return null
+    }
+
     return (
       <div className="row">
-        {this.state.pursuits.map(p => <PursuitCard {...p} />)}
+        {
+          pursuits.map(p =>
+            <PursuitCard {...p} />
+          )
+        }
       </div>
     )
   }
 }
 
-const PursuitCard = ({title, body}) =>
-  <Card className="col-md-4 col-sm-12" style={{ flex: 1, alignItems: "center", margin: 10, padding: 20 }}>
+const PursuitCard = ({title, body, link}) =>
+  <Card className="pursuit col-md-4 col-sm-12">
     <h2 className=".thin">{title}</h2>
     {body.map(el => <PursuitCardSection {...el} />)}
+    {link ? <Out link={link} /> : null}
   </Card>
 
 const PursuitCardSection = (props) => {
-  if (props.type === "graf") {
-    return <Text>{props.text}</Text>
-  } else if (props.type === "image") {
-    return <img src={props.link} alt={props.alt} style={{ maxWidth: "100%" }} />
+  switch (props.type) {
+    case "graf":
+      return <Text>{props.text}</Text>
+
+    case "image":
+      return <img alt={props.alt} src={props.link} />
   }
 }
+
+const Out = ({ link }) =>
+  <a href={link} target="_blank" className="outBtn">
+    <OutChevron />
+  </a>
+
+const OutChevron = props =>
+  <i className="fas fa-chevron-right"></i>
 
 export default Pursuits
