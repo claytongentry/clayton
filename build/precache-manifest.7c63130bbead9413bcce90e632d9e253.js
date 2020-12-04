c4643a946e27c19e984c9d531d1066b40b972d70
Clayton Gentry <cg3ntry@gmail.com>
2020-12-03 20:31:13 -0600

    Better padding across smaller devices

---
+++ build/precache-manifest.7c63130bbead9413bcce90e632d9e253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404c466ca1f69c3d3c36c5e0aa58a32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d047e5c80ac5ea3c3de8",
+    "url": "/static/css/main.32037b7e.chunk.css"
+  },
+  {
+    "revision": "523ab5d3f437c06b53a5",
+    "url": "/static/js/2.1a47c164.chunk.js"
+  },
+  {
+    "revision": "6d967876016d7f4b7c601154f6d2ed4f",
+    "url": "/static/js/2.1a47c164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d047e5c80ac5ea3c3de8",
+    "url": "/static/js/main.17e644fb.chunk.js"
+  },
+  {
+    "revision": "113aa70fbbe895759110",
+    "url": "/static/js/runtime-main.0031e58d.js"
+  }
+]);
