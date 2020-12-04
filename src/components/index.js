3af72241525031f8e33ed00291db156c9d4a652a
Clayton Gentry <cg3ntry@gmail.com>
2020-12-03 20:25:01 -0600

    Fix overflow on small screens

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Card }     from './Card'
 export { default as Menu }     from './Menu'
-export { default as MenuItem } from './MenuItem'
 export { default as Text }     from './Text'
 export { default as Title }    from './Title'
